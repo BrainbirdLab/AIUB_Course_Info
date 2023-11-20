3e18da4c56c9a18ba103326d8ddae52f4bf8c809
Fuad Hasan <fuad.cs22@gmail.com>
2023-11-20 20:36:26 +0600

    source

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+
+
+<slot></slot>
+
+<footer class="footer">
+    <div class="src">
+        <a href="https://github.com/itsfuad/AIUB_COURSE_VISUALIZER">
+            View source <i class="fa-solid fa-code"></i>
+        </a>
+    </div>
+</footer>
+
+<style>
+    footer{
+        position: absolute;
+        bottom: 0;
+        padding: 10px;
+        font-size: 0.7rem;
+        color: var(--accent);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    a{
+        color: var(--accent);
+    }
+</style>
+
