732598ff2ca8efd73a1b0fbfd7a99022b0fd5e95
Fuad Hasan <fuad.cs22@gmail.com>
2023-12-19 19:29:15 +0600

    ✨ cleanup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import "$lib/styles/global.scss";
+
+    import { onMount } from "svelte";
+
+    async function detectSWUpdate(){
+
+        const registration = await navigator.serviceWorker.ready;
+
+        registration.addEventListener("updatefound", () => {
+            const newWorker = registration.installing;
+            newWorker?.addEventListener("statechange", () => {
+                if (newWorker.state === "installed") {
+                    newWorker.postMessage({ type: "SKIP_WAITING" });
+                    console.log("New update available");
+                }
+            });
+        });
+    }
+
+    onMount(() => {
+        detectSWUpdate();
+    })
+
+</script>
+
+<slot></slot>
---
+++ src/service-worker.ts
@@ -0,0 +1,92 @@
+/// <reference types="@sveltejs/kit" />
+/// <reference lib="webworker" />
+
+import { build, files, version } from '$service-worker';
+
+declare let self: ServiceWorkerGlobalScope;
+
+const CACHE = `cache-${version}`;
+
+const ASSETS = [
+	...build,
+	...files,
+]
+
+//Call Install Event
+self.addEventListener('install', (e) => {
+	console.log('Service Worker: Installed');
+	async function addFilesToCache() {
+		const cache = await caches.open(CACHE);
+		await cache.addAll(ASSETS);
+	}
+
+	e.waitUntil(addFilesToCache());
+});
+
+//Call Activate Event
+self.addEventListener('activate', (e) => {
+	//remove previous cached data from disk
+	async function deleteOldCache() {
+		for (const key of await caches.keys()) {
+			if (key !== CACHE) await caches.delete(key);
+		}
+	}
+
+	e.waitUntil(deleteOldCache());
+});
+
+//Call Fetch Event
+self.addEventListener('fetch', (e) => {
+	//ignore POST requests
+	if (e.request.method !== 'GET') return;
+
+	async function respond(){
+		const url = new URL(e.request.url);
+		const cache = await caches.open(CACHE);
+
+		//`build`/`files` can always be served from the cache
+		if (ASSETS.includes(url.pathname)) {
+			const response = await cache.match(e.request);
+			if (response) return response;
+		}
+
+		//for everything else, try the network first, then the cache
+		//fallback to cache if offline
+		try {
+			const response = await fetch(e.request);
+
+			// if we're offline, fetch can return a value that is not a Response
+			// instead of throwing - and we can't pass this non-Response to respondWith
+			if (!(response instanceof Response)) {
+				throw new Error('invalid response from fetch');
+			}
+
+			if (response.status === 200) {
+				cache.put(e.request, response.clone());
+			}
+
+			return response;
+		} catch (err) {
+			const response = await cache.match(e.request);
+
+			if (response) {
+				return response;
+			}
+
+			// if there's no cache, then just error out
+			// as there is nothing we can do to respond to this request
+			throw err;
+		}
+	}
+
+	e.respondWith(respond());
+});
+
+
+self.addEventListener('message', (e) => {
+	//console.log(e.data);
+	if (e.data && e.data.type === 'SKIP_WAITING') {
+		console.log('Service Worker: Updated');
+		self.skipWaiting();
+	}
+});
