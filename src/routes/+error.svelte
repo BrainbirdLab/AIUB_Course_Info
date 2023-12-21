9468b91d7f4829a3b6e97177074b997fd376a045
Fuad Hasan <fuad.cs22@gmail.com>
2023-12-21 15:14:53 +0600

    error route

---
+++ src/routes/+error.svelte
@@ -0,0 +1,43 @@
+
+<div class="content">
+    <div class="message">
+        <h1>404</h1>
+        <p>Page not found</p>
+    </div>
+    <a href="/">Looking for Home?</a>
+</div>
+
+<style>
+    .content {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+        gap: 50px;
+    }
+    
+    .message {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 10px;
+    }
+
+    h1 {
+        font-size: 5rem;
+        font-weight: 700;
+        opacity: 0.15;
+    }
+
+    p {
+        font-size: 1.2rem;
+        position: absolute;
+    }
+
+
+    a {
+        color: #5482ff;
+    }        
+</style>
