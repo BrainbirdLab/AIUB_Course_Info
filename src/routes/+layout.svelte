2916d32847707110263c9971dfffa1d4bccdcc49
Fuad Hasan <fuad.cs22@gmail.com>
2024-03-28 03:35:54 +0600

    Refactor global styles and layout footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,36 +27,3 @@
 <svelte:body on:contextmenu|preventDefault />
 
 <slot></slot>
-
-<footer class="footer">
-    <div class="source"><a href="https://github.com/itsfuad/AIUB_Solution">Source code <i class="fa-solid fa-code"></i></a></div>
-    ・
-    <a href="https://www.flaticon.com/free-icons/course" title="course icons">Flaticon</a>
-    ・
-    <a href="https://www.fontawesome.com" title="fontawesome">Fontawesome</a>
-</footer>
-
-<style lang="scss">
-    .footer{
-		display: flex;
-		flex-direction: row;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		padding: 10px;
-		gap: 5px;
-		font-size: 0.6rem;
-        position: relative;
-		*{
-			padding: 0;
-			color: ghostwhite;
-		}
-
-		a{
-			text-decoration: underline;
-		}
-		i{
-			font-size: 0.7rem;
-		}
-	}
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -405,9 +405,38 @@
 			</div>
 		{/if}
 	{/if}
+	<footer class="footer">
+		<div class="source"><a href="https://github.com/itsfuad/AIUB_Solution">Source code <i class="fa-solid fa-code"></i></a></div>
+		・
+		<a href="https://www.flaticon.com/free-icons/course" title="course icons">Flaticon</a>
+		・
+		<a href="https://www.fontawesome.com" title="fontawesome">Fontawesome</a>
+	</footer>
 </div>
 
 <style lang="scss">
+	.footer{
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		padding: 10px;
+		gap: 5px;
+		font-size: 0.6rem;
+        position: relative;
+		*{
+			padding: 0;
+			color: ghostwhite;
+		}
+
+		a{
+			text-decoration: underline;
+		}
+		i{
+			font-size: 0.7rem;
+		}
+	}
 	.modalwrapper {
 		display: flex;
 		flex-direction: column;
@@ -595,7 +624,7 @@
 		align-items: center;
 		justify-content: flex-start;
 		width: 100%;
-		min-height: 100vh;
+		min-height: 100%;
 
 		.moto {
 			display: flex;
