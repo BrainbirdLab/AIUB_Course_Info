9dfbd2841979f0644d7358e749f257e18fb3e70e
Fuad Hasan <fuad.cs22@gmail.com>
2025-03-04 23:51:06 +0600

    Fix time mapping for lectures in Routine component; adjust end time for 10:20 AM - 11:20 AM slot

--- src/lib/components/Routine.svelte
+++ src/lib/components/Routine.svelte
@@ -60,8 +60,8 @@
             //Lecture (1 hour) Reduced to 40 mins
             "08:00 AM - 09:00 AM": "09:00 AM - 09:40 AM",
             "09:10 AM - 10:10 AM": "09:40 AM - 10:20 AM",
-            "10:20 AM - 11:20 AM": "11:00 AM - 11:00 AM",
-            "11:30 AM - 12:30 PM": "11:10 PM - 11:50 PM",
+            "10:20 AM - 11:20 AM": "10:20 AM - 11:00 AM",
+            "11:30 AM - 12:30 PM": "11:10 AM - 11:50 AM",
             "12:40 PM - 01:40 PM": "12:00 PM - 12:40 PM",
             "01:50 PM - 02:50 PM": "12:40 PM - 01:20 PM",
             //Laboratory Class      Lab Class Reduced Timing 
