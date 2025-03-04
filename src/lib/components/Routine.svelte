65bd29eabfef3e16127a8cb77b34fdc0a3bff7c5
Fuad Hasan <fuad.cs22@gmail.com>
2025-03-04 18:52:05 +0600

    Update time mapping for lectures and labs in Routine component

--- src/lib/components/Routine.svelte
+++ src/lib/components/Routine.svelte
@@ -43,7 +43,7 @@
 
         if (!toRamadan) return time;
 
-        //add am pm
+        
         const timeMap: Record<string, string> = {
             //Lecture (1.5 hour) Reduced to 1 Hour
             "08:00 AM - 09:30 AM": "09:00 AM - 10:00 AM",
@@ -59,11 +59,11 @@
             "03:00 PM - 05:00 PM": "01:30 PM - 02:50 PM",
             //Lecture (1 hour) Reduced to 40 mins
             "08:00 AM - 09:00 AM": "09:00 AM - 09:40 AM",
-            "09:10 AM - 10:10 AM": "10:00 AM - 10:40 AM",
-            "10:20 AM - 11:20 AM": "11:00 AM - 11:40 AM",
-            "11:30 AM - 12:30 PM": "12:00 PM - 12:40 PM",
-            "12:40 PM - 01:40 PM": "01:00 PM - 01:40 PM",
-            "01:50 PM - 02:50 PM": "02:00 PM - 02:40 PM",
+            "09:10 AM - 10:10 AM": "09:40 AM - 10:20 AM",
+            "10:20 AM - 11:20 AM": "11:00 AM - 11:00 AM",
+            "11:30 AM - 12:30 PM": "11:10 PM - 11:50 PM",
+            "12:40 PM - 01:40 PM": "12:00 PM - 12:40 PM",
+            "01:50 PM - 02:50 PM": "12:40 PM - 01:20 PM",
             //Laboratory Class      Lab Class Reduced Timing 
             "08:00 AM - 10:20 AM": "09:00 AM - 10:30 AM",
             "10:20 AM - 12:40 PM": "10:30 AM - 12:00 PM",
